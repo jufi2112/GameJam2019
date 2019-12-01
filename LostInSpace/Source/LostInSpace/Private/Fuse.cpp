5ce14e3c19cffb88ddfbb0069baab247883cd42e
Julien Fischer <jufi2112@gmail.com>
2019-12-01 15:15:49 +0100

    working on character

--- LostInSpace/Source/LostInSpace/Private/Fuse.cpp
+++ LostInSpace/Source/LostInSpace/Private/Fuse.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Fuse.h"
+#include "Components/MeshComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
 
 // Sets default values
 AFuse::AFuse()
@@ -16,11 +18,18 @@ void AFuse::BeginPlay()
 {
 	Super::BeginPlay();
 
-	if (!Light)
+	//if (!Light)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("No light component specified in %s"), *GetName())
+	//}
+	if (!MeshComponent)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("No light component specified in %s"), *GetName())
+		UE_LOG(LogTemp, Error, TEXT("Mesh component not set in %s"), *GetName())
 	}
-	
+	else
+	{
+		DynamicMaterial = MeshComponent->CreateDynamicMaterialInstance(0, FuseMaterial);
+	}
 }
 
 // Called every frame
@@ -33,6 +42,17 @@ void AFuse::Tick(float DeltaTime)
 void AFuse::SetIsActive(bool IsActive)
 {
 	bIsActive = IsActive;
+	if (DynamicMaterial)
+	{
+		if (bIsActive)
+		{
+			DynamicMaterial->SetVectorParameterValue(FName("EmissionColor"), FVector(0, 255, 0));
+		}
+		else
+		{
+			DynamicMaterial->SetVectorParameterValue(FName("EmissionColor"), FVector(0, 0, 255));
+		}
+	}
 }
 
 bool AFuse::Interact_Implementation(UObject* Caller)
@@ -42,16 +62,43 @@ bool AFuse::Interact_Implementation(UObject* Caller)
 
 bool AFuse::CanBeInteractedWith_Implementation()
 {
-	return false;
+	return bCanBeInteractedWith;
 }
 
-void AFuse::SetLightReference(UPointLightComponent* LightReference)
+void AFuse::SetMeshReference(UMeshComponent * MeshReference)
 {
-	if (!LightReference)
+	if (!MeshReference)
 	{
-		UE_LOG(LogTemp, Error, TEXT("Invalid Reference for Light specified in %s"), *GetName())
+		UE_LOG(LogTemp, Error, TEXT("Provied Mesh Reference is nullptr in %s"), *GetName())
+	}
+	else
+	{
+		MeshComponent = MeshReference;
+	}
+}
+
+void AFuse::SetTextureIndex(int32 IndexToUse, float Intensity)
+{
+	Intensity = FMath::Clamp<float>(Intensity, 0, 1);
+	if (!FuseTextures.IsValidIndex(IndexToUse))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Specified TextureIndex in %s is not in range!"), *GetName())
 		return;
 	}
-	Light = LightReference;
+	if (MeshComponent && DynamicMaterial)
+	{
+		DynamicMaterial->SetTextureParameterValue(FName("BaseTexture"), FuseTextures[IndexToUse]);
+		DynamicMaterial->SetScalarParameterValue(FName("EmissionStrength"), Intensity);
+	}
 }
+
+//void AFuse::SetLightReference(UPointLightComponent* LightReference)
+//{
+//	if (!LightReference)
+//	{
+//		UE_LOG(LogTemp, Error, TEXT("Invalid Reference for Light specified in %s"), *GetName())
+//		return;
+//	}
+//	Light = LightReference;
+//}
 
